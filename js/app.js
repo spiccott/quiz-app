f8e309dd6d4c34952c459526dfb7578e2e778af5
Stephen Piccott <stevepiccott@gmail.com>
2015-07-21 14:27:39 -0230

    Adds beginning game functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 
 $(document).ready(function() {
 
+	var score = 0;
+
+
+	$("#start-button").click(function() {
+		$(".instructions").hide();
+		$(".questions").show();
+		$("#question-one").show();
+		$(".progress-bar li:nth-child(1)").toggleClass("active").toggleClass("complete");
+		$(".progress-bar li:nth-child(2)").toggleClass("active");
+		// startQuiz();
+	});
+
+	// function startQuiz() {
+	// 	$(".instructions").hide();
+	// }
+
+	$("#question-one form").submit(function() {
+		if ()
+		$("")
+	});
 });
