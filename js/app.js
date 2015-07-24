3b1e79c3f14603534e83c19ebcd913098a3b5407
Stephen Piccott <stevepiccott@gmail.com>
2015-07-24 14:22:45 -0230

    Refactors app js to process list of question objects

--- js/app.js
+++ js/app.js
@@ -1,141 +1,71 @@
-
 $(document).ready(function() {
 
 	var score = 0;
+	var question_index = 0;
+	var question = null;
+
+	function playQuiz() {
+		if ( $("input[name='userGuess']:checked").length) {
+			var guess = parseInt($("input[name='userGuess']:checked").val());
+			var answer = question.answer;
+			console.log("Your guess is: " + guess);
+			console.log("The answer is: " + answer);
+			if ( guess == answer ) {
+				score++;
+				console.log("the score is: " + score);
+			}
+			$("[name=userGuess]").removeAttr("checked");
+			question_index++;
+			showQuiz();
+		}
+	}
+
+	function showQuiz() {
+		if ( question_index<questions.length ) {
+			question = questions[question_index];
+			$(".question-header h2").text(question.name);
+			$("#question p").text(question.question);
+			$("#answer1").text(question.choices[0]);
+			$("#answer2").text(question.choices[1]);
+			$("#answer3").text(question.choices[2]);
+			$("#answer4").text(question.choices[3]);
+			$(".progress-bar li:nth-child("+parseInt(question_index+1)+")").toggleClass("active").toggleClass("complete");
+			$(".progress-bar li:nth-child("+parseInt(question_index+2)+")").toggleClass("active");
+		}
+		else {
+			$(".questions").hide();
+			$("#results").show();
+			$(".progress-bar li:nth-child(7)").toggleClass("active").toggleClass("complete");
+			$(".progress-bar li:nth-child(8)").toggleClass("active").toggleClass("complete");
+			$(".prompt>h2").text("You've got " + score + " out of 6 questions right!");
+		}
+	}
 
 	$("#start-button").click(function() {
 		$(".instructions").hide();
 		$(".questions").show();
-		$("#question-one").show();
-		$(".progress-bar li:nth-child(1)").toggleClass("active").toggleClass("complete");
-		$(".progress-bar li:nth-child(2)").toggleClass("active");
+		showQuiz();
 	});
 
-	$("#question-one form").submit(function(e) {
+	$("#question form").submit(function(e) {
 		e.preventDefault();
-		if ( $("input[name='userGuess1']:checked").length) {
-			var guess = parseInt($("input[name='userGuess1']:checked").val());
-			var answer = 4;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-one").hide();
-			$("#question-two").show();
-			$(".progress-bar li:nth-child(2)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(3)").toggleClass("active");
-			$("[name=userGuess1]").removeAttr("checked");
-		}
+		playQuiz();
+		$("#submit-button").unbind();
 	});
 
-	$("#question-two form").submit(function(e) {
-		e.preventDefault();
-		if ( $("input[name='userGuess2']:checked").length) {
-			var guess = parseInt($("input[name='userGuess2']:checked").val());
-			var answer = 3;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-two").hide();
-			$("#question-three").show();
-			$(".progress-bar li:nth-child(3)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(4)").toggleClass("active");
-			$("[name=userGuess2]").removeAttr("checked");
-		}
-	});
-
-	$("#question-three form").submit(function(e) {
-		e.preventDefault();
-		if ( $("input[name='userGuess3']:checked").length) {
-			var guess = parseInt($("input[name='userGuess3']:checked").val());
-			var answer = 1;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-three").hide();
-			$("#question-four").show();
-			$(".progress-bar li:nth-child(4)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(5)").toggleClass("active");
-			$("[name=userGuess3]").removeAttr("checked");
-		}
-	});
-
-	$("#question-four form").submit(function(e) {
-		e.preventDefault();
-		if ( $("input[name='userGuess4']:checked").length) {
-			var guess = parseInt($("input[name='userGuess4']:checked").val());
-			var answer = 1;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-four").hide();
-			$("#question-five").show();
-			$(".progress-bar li:nth-child(5)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(6)").toggleClass("active");
-			$("[name=userGuess4]").removeAttr("checked");
-		}
-	});
-
-	$("#question-five form").submit(function(e) {
-		e.preventDefault();
-		if ( $("input[name='userGuess5']:checked").length) {
-			var guess = parseInt($("input[name='userGuess5']:checked").val());
-			var answer = 3;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-five").hide();
-			$("#question-six").show();
-			$(".progress-bar li:nth-child(6)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(7)").toggleClass("active");
-			$("[name=userGuess5]").removeAttr("checked");
-		}
-	});
-
-	$("#question-six form").submit(function(e) {
-		e.preventDefault();
-		if ( $("input[name='userGuess6']:checked").length) {
-			var guess = parseInt($("input[name='userGuess6']:checked").val());
-			var answer = 2;
-			console.log("Your guess is: " + guess);
-			if ( guess == answer ) {
-				score++;
-				console.log("the score is: " + score);
-			}
-			console.log("this is the bit after the if statement");
-			$("#question-six").hide();
-			$("#results").show();
-			$(".prompt>h2").text("You've got " + score + " out of 6 questions right!");
-			$(".progress-bar li:nth-child(7)").toggleClass("active").toggleClass("complete");
-			$(".progress-bar li:nth-child(8)").toggleClass("active");
-			$("[name=userGuess6]").removeAttr("checked");
-		}
-	});
-
-	$("#restart-button").click(function(){
-		score = 0
+	$("#restart-button").click(function() {
+		score = 0;
+		question_index = 0;
+		question = null;
 		$("#results").hide();
-		$("#question-one").show();
+		$(".questions").show();
+		$(".progress-bar li").each(function() {
+			$(this).toggleClass("complete");
+		});
+		$(".progress-bar li:nth-child(1)").toggleClass("active");
 		$(".progress-bar li:nth-child(8)").toggleClass("active");
-		$(".progress-bar li:nth-child(7)").toggleClass("complete");
-		$(".progress-bar li:nth-child(6)").toggleClass("complete");
-		$(".progress-bar li:nth-child(5)").toggleClass("complete");
-		$(".progress-bar li:nth-child(4)").toggleClass("complete");
-		$(".progress-bar li:nth-child(3)").toggleClass("complete");
-		$(".progress-bar li:nth-child(2)").toggleClass("active").toggleClass("complete");
+		showQuiz();
 	});
+
 });
+
---
+++ js/questions.js
@@ -0,0 +1,45 @@
+
+var question1 = {
+	name: "Question 1",
+	question: "Tempo is the _______ of music.",
+	choices: ["Beat", "Counts", "Time", "Speed"],
+	answer: 4
+};
+
+var question2 = {
+	name: "Question 2",
+	question: "A staff is made up of __ lines and spaces.",
+	choices: ["3", "4", "5", "6"],
+	answer: 3
+};
+
+var question3 = {
+	name: "Question 3",
+	question: 'Which term is used to mean "play loudly"?',
+	choices: ["Forte", "Staccato", "Legatto", "Mozzarella"],
+	answer: 1
+};
+
+var question4 = {
+	name: "Question 4",
+	question: "What are the lines (in order) on the treble clef?",
+	choices: ["e,g,b,d,f", "g,b,d,f,a", "b,d,f,e,g", "f,a,c,e"],
+	answer: 1
+};
+
+var question5 = {
+	name: "Question 5",
+	question: "Which of the following is an enharmonic equivalent to B-sharp?",
+	choices: ["A-sharp", "A-flat", "C", "C-flat"],
+	answer: 3
+};
+
+var question6 = {
+	name: "Question 6",
+	question: 'In a standard diatonic scale, the first note of the scale is considered the "tonic". Which position is the mediant?',
+	choices: ["Fourth", "Third", "Fifth", "Second"],
+	answer: 2
+};
+
+var questions = [question1, question2, question3, question4, question5, question6]
+
